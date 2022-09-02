1658b2cc402539535efac3417d131a412072dafd
pastorcoder <zuma4good@gmail.com>
2022-09-02 01:00:20 +0100

    fileUpload: {enableForPublic: true,},

--- index.js
+++ index.js
@@ -20,41 +20,38 @@ const config = {
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  fileUpload: enableForPublic =  true,
+  fileUpload: { enableForPublic: true },
   // filesAdapter: s3Adapter,
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
 
   //**** Email Verification ****//
-	/* Enable email verification */
-	verifyUserEmails: true,
-	/* The public URL of your app */
-	// This will appear in the link that is used to verify email addresses and reset passwords.
-	/* Set the mount path as it is in serverURL */
-	publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
-	/* This will appear in the subject and body of the emails that are sent */
-	appName: process.env.APP_NAME || "parseCodecraftExample", 
+  /* Enable email verification */
+  verifyUserEmails: true,
+  /* The public URL of your app */
+  // This will appear in the link that is used to verify email addresses and reset passwords.
+  /* Set the mount path as it is in serverURL */
+  publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+  /* This will appear in the subject and body of the emails that are sent */
+  appName: process.env.APP_NAME || 'parseCodecraftExample',
 
-	emailAdapter: {
-		module: 'parse-server-mailgun-adapter-template',
-		options: {
-       // Display name
-       displayName: 'Parse Server Test',
-        // The address that your emails come from
-			fromAddress: process.env.EMAIL_FROM || "test@example.com",
-       // Your domain from mailgun.com
-			domain: process.env.MAILGUN_DOMAIN || "example.com",
+  emailAdapter: {
+    module: 'parse-server-mailgun-adapter-template',
+    options: {
+      // Display name
+      displayName: 'Parse Server Test',
+      // The address that your emails come from
+      fromAddress: process.env.EMAIL_FROM || 'test@example.com',
+      // Your domain from mailgun.com
+      domain: process.env.MAILGUN_DOMAIN || 'example.com',
       // Your API key from mailgun.com
-			apiKey: process.env.MAILGUN_API_KEY  || "apikey"
-		}
-	},
-  
+      apiKey: process.env.MAILGUN_API_KEY || 'apikey',
+    },
+  },
 
   // var fs = require('fs'); //required if loading html from file!
 
-
-  
   // emailAdapter: {
   //   module: 'parse-server-mailgun-adapter-template',
   //   options: {
@@ -81,39 +78,27 @@ const config = {
   //   }
   // }
 
-
-
-
-
-	
-	//**** File Storage ****//
-	// filesAdapter: new S3Adapter(
-	// 	{
-	// 		directAccess: true //allows to load server/asset directly from S3
-	// 	}
-	// )
-
- 
-
-
+  //**** File Storage ****//
+  // filesAdapter: new S3Adapter(
+  // 	{
+  // 		directAccess: true //allows to load server/asset directly from S3
+  // 	}
+  // )
 };
 // fileUpload.enableForPublic =  true;
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
-
 // parse-dashboard --dev --appId myAppId --masterKey myMasterKey --serverURL "https://parse-from-real-source.herokuapp.com/parse" --appName parse-test
 
 // Source -  https://www.youtube.com/watch?v=MAiva7qYUbc&list=PL-TLnxxt_AVFEOlCFBHBG_BbpaF3UX-EU&index=13
-
 
 // s3
 // S3_ACCESS_KEY - AKIAY7NPONVIPH4UUC5H
 // S3_BUCKET - parse-server-codecraft-example2
 // S3_SECRET_KEY - G8O/EElTbOG50ct4e+tnAcfEEef/iVHEduY3pQVa
 // SERVER_URL - https://parse-from-real-source.herokuapp.com/parse
-
 
 const app = express();
 
